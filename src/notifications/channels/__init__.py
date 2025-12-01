03881852fa75927cce6b638dde9722266e2c98be
TeamReel Mirror <mirror@teamreel.app>
2025-12-01 21:16:21 +0100

    WP03: Export EmailChannel from channels __init__.py

--- src/notifications/channels/__init__.py
+++ src/notifications/channels/__init__.py
@@ -1,10 +1,12 @@
 """Notification delivery channels."""
 
 from .base import NotificationChannel
+from .email import EmailChannel
 from .exceptions import ChannelError, PermanentChannelError, TransientChannelError
 
 __all__ = [
     "NotificationChannel",
+    "EmailChannel",
     "ChannelError",
     "TransientChannelError",
     "PermanentChannelError",
